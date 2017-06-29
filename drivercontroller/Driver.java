bfe5ecddbd8fcbfb591d656559f941c1664354f2
steven2805 <stevenmac90@googlemail.com>
2017-06-29 20:34:42 +0100

    added the folder stucture

---
+++ drivercontroller/Driver.java
@@ -0,0 +1,16 @@
+package drivercontroller;
+
+public class Driver {
+
+
+  String name;
+
+  public Driver(String name){
+    this.name = name;
+  }
+
+  public String getName(){
+    return this.name;
+  }
+
+}
